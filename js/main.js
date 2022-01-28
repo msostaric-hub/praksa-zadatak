dc67012eace32055375631fb30383b141265eb1e
msostaric <54621273+msostaric-hub@users.noreply.github.com>
2022-01-28 16:58:06 +0100

    Commit #5
    Dodani efekti za gumbove - hover gumbova mijenja boju obruba i sliku, kada miš stoji na gumbu ono iz sivog obruba i strelice prelazi u plavu varijantu te se vraća u sivu nakon što se miš makne sa gumba.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,45 @@
 $(document).ready(function () {
-  $(".btn").hover(function () {
-    $(this).css("cursor", "pointer");
-  });
+  $(".btnR").hover(
+    function () {
+      $(this).css("cursor", "pointer");
+      $(this).css({ "border-color": "#134880" });
+      $(this).attr(
+        "src",
+        $(this)
+          .attr("src")
+          .replace("img/arrow-gray-right.png", "img/arrow-blue-right.png")
+      );
+    },
+    function () {
+      $(this).css({ "border-color": "#dddddd" });
+      $(this).attr(
+        "src",
+        $(this)
+          .attr("src")
+          .replace("img/arrow-blue-right.png", "img/arrow-gray-right.png")
+      );
+    }
+  );
+
+  $(".btnL").hover(
+    function () {
+      $(this).css("cursor", "pointer");
+      $(this).css({ "border-color": "#134880" });
+      $(this).attr(
+        "src",
+        $(this)
+          .attr("src")
+          .replace("img/arrow-gray-left.png", "img/arrow-blue-left.png")
+      );
+    },
+    function () {
+      $(this).css({ "border-color": "#dddddd" });
+      $(this).attr(
+        "src",
+        $(this)
+          .attr("src")
+          .replace("img/arrow-blue-left.png", "img/arrow-gray-left.png")
+      );
+    }
+  );
 });
