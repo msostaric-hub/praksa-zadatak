a4d522aa9ebb79333cb8637e92f3e0ddc1af75db
msostaric <54621273+msostaric-hub@users.noreply.github.com>
2022-01-30 17:02:12 +0100

    Commit #8
    Dodan Open Sans font

--- js/main.js
+++ js/main.js
@@ -46,10 +46,10 @@ $(document).ready(function () {
 
   // Gumb za pomicanje slika prema desno
   $(".btnR").click(function () {
-    var currentImg = $(".active");
-    var nextImg = currentImg.next();
-    var imgSelector = document.querySelector(".active");
-    var imgWidth = imgSelector.offsetWidth;
+    let currentImg = $(".active");
+    let nextImg = currentImg.next();
+    let imgSelector = document.querySelector(".active");
+    let imgWidth = imgSelector.offsetWidth;
     if (nextImg.length) {
       currentImg.removeClass("active");
       $(".slider1").animate({ right: imgWidth });
@@ -59,10 +59,10 @@ $(document).ready(function () {
   });
   // Gumb za pomicanje slika prema lijevo
   $(".btnL").click(function () {
-    var currentImg = $(".active");
-    var nextImg = currentImg.next();
-    var imgSelector = document.querySelector(".active");
-    var imgWidth = imgSelector.offsetWidth;
+    let currentImg = $(".active");
+    let nextImg = currentImg.next();
+    let imgSelector = document.querySelector(".active");
+    let imgWidth = imgSelector.offsetWidth;
     if (nextImg.length) {
       currentImg.removeClass("active");
       $(".slider1").animate({ left: imgWidth });
