23f93204d0af6246b0b452374f94b5826fa0d278
msostaric <54621273+msostaric-hub@users.noreply.github.com>
2022-01-30 16:41:53 +0100

    Commit #7
    Dodana funkcija za pomicanje slika. Širinu slike za koju se trebaju slike pomaknuti sam uzeo preko querySelector-a i offsetWidth te dobivenu dužinu spremio u varijablu i tako stavio u funkciju. Pošto moje znanje jQuery-a je još početno, ne znam kako napraviti da se slike pravilno pomiču svakim klikom na gumb.

--- js/main.js
+++ js/main.js
@@ -44,26 +44,30 @@ $(document).ready(function () {
     }
   );
 
+  // Gumb za pomicanje slika prema desno
   $(".btnR").click(function () {
-    $(".slider1").css({ transform: "translateX(150px)" });
-    $(".slider2").css({ transform: "translateX(150px)" });
+    var currentImg = $(".active");
+    var nextImg = currentImg.next();
+    var imgSelector = document.querySelector(".active");
+    var imgWidth = imgSelector.offsetWidth;
+    if (nextImg.length) {
+      currentImg.removeClass("active");
+      $(".slider1").animate({ right: imgWidth });
+      $(".slider2").animate({ right: imgWidth });
+      nextImg.addClass("active");
+    }
   });
+  // Gumb za pomicanje slika prema lijevo
   $(".btnL").click(function () {
-    $(".slider1").css({ transform: "translateX(-150px)" });
-    $(".slider2").css({ transform: "translateX(-150px)" });
+    var currentImg = $(".active");
+    var nextImg = currentImg.next();
+    var imgSelector = document.querySelector(".active");
+    var imgWidth = imgSelector.offsetWidth;
+    if (nextImg.length) {
+      currentImg.removeClass("active");
+      $(".slider1").animate({ left: imgWidth });
+      $(".slider2").animate({ left: imgWidth });
+      nextImg.addClass("active");
+    }
   });
 });
-
-// var count = $(".content1").length; //length gives total length of elements
-// $("#total").text(count);
-// if ($current.is($slides.last())) {
-//   $("#current").text("1"); //first slide
-//   $current.hide();
-//   $slides.first().show();
-// }
-// // else, hide current slide and show the next one
-// else {
-//   $("#current").text($current.next().index() + 1);
-//   //index() returns index, add 1 because it starts from 0
-//   $current.hide().next().show();
-// }
