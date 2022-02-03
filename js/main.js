b7c10d9555a7d1e43630d2f87885ec23f3a79f84
msostaric <54621273+msostaric-hub@users.noreply.github.com>
2022-02-03 18:01:19 +0100

    Commit #8
    Gumbovi promijenjeni u button tagove

--- js/main.js
+++ js/main.js
@@ -1,49 +1,5 @@
 "use strict";
 $(document).ready(function () {
-  $(".btnR").hover(
-    function () {
-      $(this).css("cursor", "pointer");
-      $(this).css({ "border-color": "#134880" });
-      $(this).attr(
-        "src",
-        $(this)
-          .attr("src")
-          .replace("img/arrow-gray-right.png", "img/arrow-blue-right.png")
-      );
-    },
-    function () {
-      $(this).css({ "border-color": "#dddddd" });
-      $(this).attr(
-        "src",
-        $(this)
-          .attr("src")
-          .replace("img/arrow-blue-right.png", "img/arrow-gray-right.png")
-      );
-    }
-  );
-
-  $(".btnL").hover(
-    function () {
-      $(this).css("cursor", "pointer");
-      $(this).css({ "border-color": "#134880" });
-      $(this).attr(
-        "src",
-        $(this)
-          .attr("src")
-          .replace("img/arrow-gray-left.png", "img/arrow-blue-left.png")
-      );
-    },
-    function () {
-      $(this).css({ "border-color": "#dddddd" });
-      $(this).attr(
-        "src",
-        $(this)
-          .attr("src")
-          .replace("img/arrow-blue-left.png", "img/arrow-gray-left.png")
-      );
-    }
-  );
-
   // Gumb za pomicanje slika prema desno
   $(".btnR").click(function () {
     let currentImg = $(".active");
